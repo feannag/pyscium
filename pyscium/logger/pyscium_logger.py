c2a8bd366939e7d63f008c7459a0a02f62444754
flint <nova.parker@gmx.com>
2020-01-22 03:55:18 -0500

    Refactor: refactor get_logger()

--- pyscium/logger/pyscium_logger.py
+++ pyscium/logger/pyscium_logger.py
@@ -4,10 +4,11 @@ import logging
 def get_logger(name, filename):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
     fh = logging.FileHandler(filename='./logs/' + filename, mode='w')
+    formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
     fh.setFormatter(formatter)
+
     logger.addHandler(fh)
 
     return logger
